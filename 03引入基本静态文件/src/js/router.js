3814e52bd7aa6ddd94b24ed2525e03b1dfcc77f5
tinymark <crazybigmoon@gmail.com>
2017-08-13 16:34:10 +0800

    03样式文件

---
+++ 03引入基本静态文件/src/js/router.js
@@ -0,0 +1,17 @@
+import VueRouter from 'vue-router';
+
+import home from '../components/home.vue';
+import match from '../components/match.vue';
+import video from '../components/video.vue';
+import strategy from '../components/strategy.vue';
+
+export default new VueRouter({
+    routes: [
+        { path: '/', redirect: '/home' },
+        { path: '/home', component: home },
+        { path: '/match', component: match },
+        { path: '/video', component: video },
+        { path: '/strategy', component: strategy }
+    ],
+    linkActiveClass: 'currentL'
+})
