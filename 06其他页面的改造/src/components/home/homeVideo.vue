fb57440d0ee746eee0ccc9d8849d22f7a627f4a0
tinymark <crazybigmoon@gmail.com>
2017-08-14 12:03:06 +0800

    06其他页面改造

---
+++ 06其他页面的改造/src/components/home/homeVideo.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="index_cont">
+        <div class="index_cont_title">
+            <span class="ico_video"></span>
+            <h3>精彩视频</h3>
+            <a href="http://pvp.qq.com/m/m201606/detail.shtml?G_Biz=18&tid=145262&e_code=pvpapp.targetId_0.r0" onclick="pgvSendClick({hottag: 'mobile.moreNav.videos'});" class="ico_more">more</a>
+        </div>
+        <ul class="video_list clearfix" id="index_video_list">
+            <li>
+                <a href="">
+                    <img src="http://shp.qpic.cn/qqvideo_ori/0/i034295c1v3_228_128/0" alt="【荣耀时刻】Shadow强势五杀 联赛时长再刷新">
+                    <span class="ico_play"></span>
+                    <span class="video_title">KPL第一周 ESTAR气势KPL第一周 </span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="http://shp.qpic.cn/qqvideo_ori/0/i034295c1v3_228_128/0" alt="【荣耀时刻】Shadow强势五杀 联赛时长再刷新">
+                    <span class="ico_play"></span>
+                    <span class="video_title">KPL第一周 ESTAR气势</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ 06其他页面的改造/src/components/match/match.vue
@@ -0,0 +1,91 @@
+<template>
+    <section id="content_wrap">
+        <div class="mc_block">
+            <div class="mc_descinfo">
+                <p class="mc_dinfo">
+                    <span class="mc_descinfo_title">KPL 王者职业联赛</span>
+                    <span class="mc_descinfo_type">官方举办</span>
+                </p>
+                <p class="mc_data">
+                    <span>2017-03-24</span> 至
+                    <span>2017-07-08</span>
+                </p>
+            </div>
+        </div>
+        <div class="mc_lineinfo">
+            <span class="mc_w">进行中</span>
+        </div>
+    
+        <!--video-->
+        <div class="mc_video">
+            <img class="video_img" src="../../img/kpl.jpg" alt="KPL 王者职业联赛">
+            <div class="video_mask">
+                <span class="video_btn_play"></span>
+            </div>
+        </div>
+        <div class="mc_allB mc_allB_news">赛事资讯</div>
+        <div class="mc_gamemsg">
+            <div class="marchLiCont">
+                <div class="ac_tap mess_list newList_MarginTop">
+                    <ul>
+                        <li>
+                            <a href="javascript:">王者论英雄-第15期：Evildoer教你玩狄仁杰</a>
+                            <time>06/22</time>
+                        </li>
+                        <li>
+                            <a href="javascript:">第三届王者荣耀校园争霸赛总决赛回顾</a>
+                            <time>06/22</time>
+                        </li>
+                        <li>
+                            <a href="javascript:">战报：SC让一追二胜SV Best险胜Hero夺积分榜第一</a>
+                            <time>06/22</time>
+                        </li>
+                        <li>
+                            <a href="javascript:">快讯：安然高渐离发挥亮眼 SC2:1SV再夺一份</a>
+                            <time>06/22</time>
+                        </li>
+                        <li>
+                            <a href="javascript:">【KPL李九赛前预测】相比之下AG超玩会赢面大</a>
+                            <time>06/22</time>
+                        </li>
+                        <li>
+                            <a href="javascript:">高输出经济比，Cat季后赛中单稳定如一</a>
+                            <time>06/22</time>
+                        </li>
+                        <li>
+                            <a href="javascript:">【6.20KPL预选赛】BEST2：1拿下HERO，实现梦幻四连胜</a>
+                            <time>06/22</time>
+                        </li>
+                        <li>
+                            <a href="javascript:">【KPL四强战队采访】QGhappy目标冠军，Cat期待与伪装一战！</a>
+                            <time>06/22</time>
+                        </li>
+                        <li>
+                            <a href="javascript:">【KPL预选赛】BA2：0横扫SV，领跑积分榜</a>
+                            <time>06/21</time>
+                        </li>
+                        <li>
+                            <a href="javascript:">AG超玩会实力取胜，无惧任何套路</a>
+                            <time>06/21</time>
+                        </li>
+                        <li>
+                            <h3 class="ac_move" data-pagenow="1" data-pagetotal="143" data-pagenext="/webplat/info/news_version3/15592/24418/24853/m16639/list_2.shtml">
+                                上拉加载更多
+                            </h3>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+@import url('../../css/match.css');
+</style>
