772f2a935cb93adaba6b683830198171b03ca3d8
tinymark <crazybigmoon@gmail.com>
2017-08-18 01:21:01 +0800

    05主页内容部分

---
+++ 05主页内容部分/src/components/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="wrap">
+        <v-homeTop> </v-homeTop>
+        <router-view></router-view>
+        <v-homeBottom> </v-homeBottom>
+    </div>
+</template>
+
+<script>
+import homeTop from './home/homeTop.vue';
+import homeBottom from './home/homeBottom.vue';
+export default {
+    components: {
+        'v-homeTop': homeTop,
+        'v-homeBottom': homeBottom
+    }
+}
+</script>
+
+<style lang='less'>
+@import '../style/base.less';
+</style>
