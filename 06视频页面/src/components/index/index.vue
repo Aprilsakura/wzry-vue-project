150441ad4b1024045f89fe7004faf860e12add4f
tinymark <crazybigmoon@gmail.com>
2017-08-18 13:04:57 +0800

    06视频页面

---
+++ 06视频页面/src/components/index/index.vue
@@ -0,0 +1,64 @@
+<template>
+    <section class='index'>
+        <!-- 轮播图start -->
+        <v-swipe :swipeList="swipeList"></v-swipe>
+        <!-- 轮播图end -->
+    
+        <!-- 主页导航start -->
+        <v-homeNav></v-homeNav>
+        <!-- 主页导航end -->
+    
+        <!-- 主页内容start -->
+        <v-iContent v-for='(item,i) in contList' :key='i' :type='item'></v-iContent>
+        <!-- 主页内容end -->
+    </section>
+</template>
+
+<script>
+import config from '../../js/config.js';
+import iSwiper from '../swipe/swipe.vue';
+import iHomeNav from './homeNav.vue';
+import iContent from './homeCont.vue';
+
+export default {
+    data() {
+        return {
+            'swipeList': [],
+            'contList': [
+                { tab: 'news', msg: '热点资讯', href: 'http://pvp.qq.com/m/m201606/newsList.shtml' },
+                { tab: 'video', msg: '精彩视频', href: 'http://pvp.qq.com/m/m201706/strategycenter.shtml' },
+                { tab: 'strategy', msg: '精品攻略', href: 'http://pvp.qq.com/m/m201606/strategy.shtml' }
+            ]
+        }
+    },
+    methods: {
+        getLunbo() {
+            let url = config.getHomeLunbo;
+            this.$http.get(url).then(rep => {
+                let body = rep.body;
+                if (body.status == 0) this.swipeList = body.message;
+            });
+        }
+    },
+    components: {
+        'v-swipe': iSwiper,
+        'v-homeNav': iHomeNav,
+        'v-iContent': iContent
+    },
+    created() {
+        this.getLunbo();
+    },
+    updated() {
+        new Swiper('.swiper-container', {
+            pagination: '.swiper-pagination',
+            paginationClickable: true,
+            autoplay: 3000,
+            loop: true
+        });
+    }
+}
+</script>
+
+<style lang='less'>
+@import '../../style/index.less';
+</style>
---
+++ 06视频页面/src/js/config.js
@@ -0,0 +1,12 @@
+const domain = 'http://139.199.192.48:9092';
+export default {
+    domin: domain,
+    getHomeLunbo: domain + '/api/getlunbo?type=home',
+    getHomeNav: domain + '/api/getnavlist',
+    getNews: domain + '/api/getnews?type=公告',
+    getVideos: domain + '/api/getvideos',
+    getRaiders: domain + '/api/getraiders',
+
+    getVieoLunbo: domain + '/api/getlunbo?type=video',
+
+}
