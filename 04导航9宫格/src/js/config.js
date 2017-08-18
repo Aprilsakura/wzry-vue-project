d69a1e11d06e6fb5bb482ef3617263030789b1ca
tinymark <crazybigmoon@gmail.com>
2017-08-19 02:26:53 +0800

    gh-pages

--- 04导航9宫格/src/js/config.js
+++
@@ -1,7 +0,0 @@
-const domain = 'http://139.199.192.48:9092';
-export default {
-    domin: domain,
-    getHomeLunbo: domain + '/api/getlunbo?type=home',
-    getHomeNav: domain + '/api/getnavlist',
-    
-}
